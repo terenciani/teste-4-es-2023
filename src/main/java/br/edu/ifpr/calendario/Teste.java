936b758e8ebda4986e619c4794ae63edcfa78001
Marcelo F. Terenciani <marcelo.terenciani@gmail.com>
2023-05-17 14:18:30 -0300

    Adição do calendário no pom

---
+++ src/main/java/br/edu/ifpr/calendario/Teste.java
@@ -0,0 +1,9 @@
+package br.edu.ifpr.calendario;
+
+import paranavai.calendario.Calendario;
+
+public class Teste {
+    public static void main(String[] args) {
+        Calendario.mostrarCalendario();
+    }
+}
